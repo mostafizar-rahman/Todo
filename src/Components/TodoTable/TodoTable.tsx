73f087cfbc22dadb2cbd3c58b350eac4ce688944
Mostafizur <mostafizurdh2000@gmail.com>
2023-08-18 21:19:42 +0600

    bug fix 6

--- src/Components/TodoTable/TodoTable.tsx
+++ src/Components/TodoTable/TodoTable.tsx
@@ -22,7 +22,7 @@ const TodoTable = () => {
   useEffect(() => {
     const todos = localStorageGetTodo();
 
-    if (todos) {
+    if (todos.length < 0) {
       // First time load localstorge todos list then when add a new todo it load on time
       setTodoList(todos || todoTitems);
       setLoading(false);
